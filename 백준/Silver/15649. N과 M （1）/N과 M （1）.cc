18e654145b59e0670db4dcd6eb58fbe424ab7939
uiopyt12 <108012256+uiopyt12@users.noreply.github.com>
2024-07-15 10:51:46 +0900

    [Silver III] Title: N과 M (1), Time: 24 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.cc
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int n, m;
+int arr[10];
+bool isUsed[10];
+void permutation(int k)
+{
+	if (k == m)
+	{
+		for (int i = 0; i < k; i++)
+			cout << arr[i]+1 << ' ';
+		cout << '\n';
+		return;
+	}
+	
+	for (int i = 0; i < n; i++)
+	{
+		if (isUsed[i] == true)
+			continue;
+		
+		arr[k] = i;
+		isUsed[i] = true;
+		permutation(k + 1);
+		isUsed[i] = false;
+	}
+	
+
+}
+
+
+int main()
+{
+	cin >> n >> m;
+	permutation(0);
+}
