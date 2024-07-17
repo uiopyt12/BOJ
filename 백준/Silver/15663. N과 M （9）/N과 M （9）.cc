437371fa2fd54523a630d41b4cd0fa23fb9cc98d
uiopyt12 <108012256+uiopyt12@users.noreply.github.com>
2024-07-17 13:36:35 +0900

    [Silver II] Title: N과 M (9), Time: 12 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/15663. N과 M （9）/N과 M （9）.cc
@@ -0,0 +1,54 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int n, m;
+int ans[10];
+int num[10]; 
+int freq[10]; // 0 to 9
+
+void permut(int level)
+{
+	if (level == m)
+	{
+		for (int i = 0; i < level; i++)
+			cout << ans[i] << ' ';
+		cout << '\n';
+	}
+
+	for (int i = 0; i < 10; i++)
+	{
+		if (freq[i] == 0) continue;
+		ans[level] = num[i];
+		freq[i]--;
+		permut(level + 1);
+		freq[i]++;
+	}
+}
+
+int main()
+{
+	cin >> n >> m;
+	
+	for (int i = 0; i < n; i++)
+		cin >> num[i];
+	sort(num, num + n);
+
+
+	for (int i = 0; i < n; i++)
+	{
+		if (num[i] == 0) break;
+		freq[i]++;
+		if (num[i] == num[i + 1])
+		{
+			int k = i;
+			while (num[k] == num[k + 1])
+			{
+				k++;
+				freq[i]++;
+			}
+			i = k;
+		}
+	}
+
+	permut(0);
+}
