43314e3188b2bddb0cc270dddd83d88ecee46683
uiopyt12 <108012256+uiopyt12@users.noreply.github.com>
2024-10-01 15:39:32 +0900

    [Silver V] Title: 좌표 정렬하기, Time: 40 ms, Memory: 2808 KB -BaekjoonHub

---
+++ 백준/Silver/11650. 좌표 정렬하기/좌표 정렬하기.cc
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+bool compare(pair<int, int> a, pair<int, int> b)
+{
+	if (a.first == b.first) return a.second < b.second;
+	return a.first < b.first;
+}
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+
+	int n;
+	cin >> n;
+	vector<pair<int, int>> v1(n);
+
+	for (int i=0; i<n; i++)
+	{
+		cin >> v1[i].first >> v1[i].second;
+	}
+
+	sort(v1.begin(), v1.end(), compare);
+
+	for (int i = 0; i < n; i++)
+	{
+		cout << v1[i].first << ' ' << v1[i].second << '\n';
+	}
+}
