f59e6fb0062958c15853a708bd5cfe69a8183d7f
uiopyt12 <108012256+uiopyt12@users.noreply.github.com>
2025-08-01 10:10:44 +0900

    [Bronze V] Title: 연세대학교, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/15680. 연세대학교/연세대학교.c
@@ -0,0 +1,13 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+
+int main()
+{
+	int n;
+	scanf("%d", &n);
+	if (n == 0)
+		printf("YONSEI");
+	else
+		printf("Leading the Way to the Future");
+}
